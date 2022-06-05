ec99be854d5f229447b5af77ab2b6d10c0686d85
italsehf <alsehfdl0316@naver.com>
2022-06-06 01:34:16 +0900

    "Before Save Memo"

---
+++ Shared/MalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MalendarApp.swift
+//  Shared
+//
+//  Created by Keum MinSeok on 2022/04/25.
+//
+
+import SwiftUI
+
+@main
+struct MalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shared/Model/Memo.swift
@@ -0,0 +1,67 @@
+//
+//  Memo.swift
+//  Malendar (iOS)
+//
+//  Created by Keum MinSeok on 2022/05/02.
+//
+
+import SwiftUI
+
+// Memo Model and Sample Memo
+// Array of memos
+struct Memo: Identifiable{
+    var id = UUID().uuidString
+    var title: String
+    var time: Date = Date()
+}
+
+// Total Memo Meta View
+struct MemoMetaData: Identifiable{
+    var id = UUID().uuidString
+    var memo: [Memo]
+    var memoDate: Date
+}
+
+//sample Date for Testing
+func getSampleDate(offset: Int)->Date{
+    let calendar = Calendar.current
+    
+    let date = calendar.date(byAdding: .day, value: offset, to: Date())
+    
+    return date ?? Date()
+}
+
+// Sample Memos
+var memos: [MemoMetaData] = [
+
+    MemoMetaData(memo: [
+    
+        Memo(title: "NC1, The End"),
+        Memo(title: "Get some rest😵"),
+        Memo(title: "Pokemon Bread Ticketing")
+    ], memoDate: getSampleDate(offset: 0)),
+    MemoMetaData(memo: [
+    
+        Memo(title: "Talk to Rey")
+    ], memoDate: getSampleDate(offset: -3)),
+    MemoMetaData(memo: [
+        
+        Memo(title: "Meeting with Lingo")
+    ], memoDate: getSampleDate(offset: -8)),
+    MemoMetaData(memo: [
+    
+        Memo(title: "SwiftUI WorkShop")
+    ], memoDate: getSampleDate(offset: 10)),
+    MemoMetaData(memo: [
+
+        Memo(title: "Get some rest😵")
+    ], memoDate: getSampleDate(offset: -22)),
+    MemoMetaData(memo: [
+
+        Memo(title: "Nothing Much Workout")
+    ], memoDate: getSampleDate(offset: 15)),
+    MemoMetaData(memo: [
+        
+        Memo(title: "Design Workshop")
+    ], memoDate: getSampleDate(offset: -20)),
+ ]
